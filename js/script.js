28263744b06a11796b3eb8927d6bafc94f6b1ba9
Marlon Lucas <marlon@MacBook-Pro-de-Marlon.local>
2025-04-06 21:51:12 -0600

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -7,36 +7,62 @@ const btnVerificar = document.getElementById("verificar");
 const mensaje = document.getElementById("mensaje");
 const btnCerrar = document.getElementById("cerrar");
 
-
-
-
 //Constantes a cada botón
 const btnParImpar = document.getElementById("btnParImpar");
 const btnMayorEdad = document.getElementById("btnMayorEdad");
-const btnPositivoNegativo =document.getElementById("btnPositivoNegativo")
-const btnCalificacion = document.getElementById("btnCalificacion")
-
+const btnPositivoNegativo = document.getElementById("btnPositivoNegativo");
+const btnCalificacion = document.getElementById("btnCalificacion");
+const btnConteo = document.getElementById("btnConteo");
+const btnTabla = document.getElementById("btnTabla");
 
 let funcionEjercicioActual = null;
 
 //Eventos
-btnParImpar.addEventListener("click",function(){
-  abrirModal("Número Par o Impar","Ingresa un número para verificar si es par o impar", verificarParImpar);
+btnParImpar.addEventListener("click", function () {
+  abrirModal(
+    "Número Par o Impar",
+    "Ingresa un número para verificar si es par o impar",
+    verificarParImpar
+  );
 });
 
-btnMayorEdad.addEventListener("click",function(){
-  abrirModal ("Mayor de Edad","Ingresa tu edad para saber si eres mayor de edad",verificarEdad);
+btnMayorEdad.addEventListener("click", function () {
+  abrirModal(
+    "Mayor de Edad",
+    "Ingresa tu edad para saber si eres mayor de edad",
+    verificarEdad
+  );
 });
 
-btnPositivoNegativo.addEventListener("click", function(){
-  abrirModal ("Positivo o Negativo", "Ingresa un numero",NumPositivoNegativo);
-})
+btnPositivoNegativo.addEventListener("click", function () {
+  abrirModal("Positivo o Negativo", "Ingresa un numero", NumPositivoNegativo);
+});
 
-btnCalificacion.addEventListener("click", function(){
-  abrirModal("Sistema de Calificaciones", "Ingresa tu nota de 1 a 10", Calificacion)
-})
+btnCalificacion.addEventListener("click", function () {
+  abrirModal(
+    "Sistema de Calificaciones",
+    "Ingresa tu nota de 1 a 10",
+    Calificacion
+  );
+});
 
-function abrirModal(titulo, descripcion, funcionEjercicio){
+btnConteo.addEventListener("click", function () {
+  abrirModal(
+    "Conteo de numeros",
+    "Coloca el numero hasta donde quieres contar",
+    conteo
+  );
+});
+
+btnTabla.addEventListener("click", function () {
+  abrirModal(
+    "Tablas de multiplicar",
+    "Consulta la tabla de tu eleccion: ",
+    Tabla
+  );
+});
+
+function abrirModal(titulo, descripcion, funcionEjercicio) {
   tituloModal.textContent = titulo;
   ejercicioModal.textContent = descripcion;
   respuesta.value = "";
@@ -45,61 +71,86 @@ function abrirModal(titulo, descripcion, funcionEjercicio){
   modal.classList.add("activarModal");
 }
 
-function verificarParImpar(){
-  let numero = parseInt (respuesta.value);
-  if (isNaN(numero)){
-  mensaje.textContent = "Ingresa un número válido.";
-  }
-  else{
-    mensaje.textContent = (numero % 2 === 0) ? "Es un número PAR" : "Es un número IMPAR";
+function verificarParImpar() {
+  let numero = parseInt(respuesta.value);
+  if (isNaN(numero)) {
+    mensaje.textContent = "Ingresa un número válido.";
+  } else {
+    mensaje.textContent =
+      numero % 2 === 0 ? "Es un número PAR" : "Es un número IMPAR";
   }
 }
 
-function verificarEdad(){
-  let numero= parseInt(respuesta.value)
+function verificarEdad() {
+  let numero = parseInt(respuesta.value);
 
-  if (isNaN(numero)){
+  if (isNaN(numero)) {
     mensaje.textContent = "Ingresa tu edad.";
-    }
-    else{
-      mensaje.textContent = (numero >=18) ? "Eres Mayor de edad" : "Eres menor de edad";
-    }
-}
-
-function NumPositivoNegativo(){
-  let num= respuesta.value;
-  if(isNaN(num)){
-    mensaje.textContent ="Ingresa un numero"
-  }
-  else{
-    mensaje.textContent = (num>=0)? "El numero es positivo":"Numero es negativo"
+  } else {
+    mensaje.textContent =
+      numero >= 18 ? "Eres Mayor de edad" : "Eres menor de edad";
   }
 }
 
-function Calificacion(){
+function NumPositivoNegativo() {
+  let num = respuesta.value;
+  if (isNaN(num)) {
+    mensaje.textContent = "Ingresa un numero";
+  } else {
+    mensaje.textContent =
+      num >= 0 ? "El numero es positivo" : "Numero es negativo";
+  }
+}
+
+function Calificacion() {
   let calificacion = respuesta.value;
 
-if( calificacion < 5 && calificacion >= 0 ){
-    mensaje.textContent="Calificación: Insuficiente";
-}else if( calificacion === 5 ){
-    mensaje.textContent="Calificación: Suficiente";
-}else if( calificacion > 5 && calificacion <= 8 ){
-    mensaje.textContent= "Calificación: Bien";
-}else if( calificacion > 8 && calificacion <= 10 ){
-    mensaje.textContent="Calificación: Excelente";
-}else{
-    mensaje.textContent="Calificación no valida";
+  if (calificacion < 5 && calificacion >= 0) {
+    mensaje.textContent = "Calificación: Insuficiente";
+  } else if (calificacion === 5) {
+    mensaje.textContent = "Calificación: Suficiente";
+  } else if (calificacion > 5 && calificacion <= 8) {
+    mensaje.textContent = "Calificación: Bien";
+  } else if (calificacion > 8 && calificacion <= 10) {
+    mensaje.textContent = "Calificación: Excelente";
+  } else {
+    mensaje.textContent = "Calificación no valida";
+  }
 }
+
+function conteo() {
+  let numero = respuesta.value;
+  let contador,
+    res = "";
+
+  for (contador = 1; contador < numero; contador++) {
+    res += contador + " , ";
+  }
+  res += contador;
+  mensaje.textContent = res;
 }
 
+function Tabla() {
+  let dato = respuesta.value;
+  let resultado,
+    contador,
+    prefijo,
+    producto = " ";
 
+  prefijo = "\n" + dato + " x " + "\n";
+  for (contador = 1; contador <= 10; contador++) {
+    producto = dato * contador;
+    resultado += "\n" + prefijo + contador + " = " + producto + "\n" + "\n";
+  }
+  mensaje.textContent = "Tabla del " + dato + resultado;
+}
 
-btnVerificar.addEventListener("click",function(){
-  if(funcionEjercicioActual){
-    funcionEjercicioActual ();
+btnVerificar.addEventListener("click", function () {
+  if (funcionEjercicioActual) {
+    funcionEjercicioActual();
   }
 });
 
-btnCerrar.addEventListener("click",function(){
-  modal.classList.remove("activarModal")
+btnCerrar.addEventListener("click", function () {
+  modal.classList.remove("activarModal");
 });
